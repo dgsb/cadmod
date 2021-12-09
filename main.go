fe1d56b61a2b8b392e38921707ab9a7872900aec
David Bariod <dbariod@scaleway.com>
2021-12-09 14:40:41 +0100

    add simple working example with a reverse proxy which adds an arbitrary header to the request

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package cadmod
 
 import (
+	"net/http"
+
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+)
+
+var (
+	_ caddyfile.Unmarshaler = CadMod{}
 )
 
 type CadMod struct{}
@@ -15,6 +24,24 @@ func (CadMod) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
+func (c CadMod) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+	}
+	return nil
+}
+
+func (c CadMod) ServeHTTP(wr http.ResponseWriter, req *http.Request, h caddyhttp.Handler) error {
+	req.Header.Add("X-fake-added-header", "I'm fake !!")
+	return h.ServeHTTP(wr, req)
+}
+
 func init() {
 	caddy.RegisterModule(CadMod{})
+	httpcaddyfile.RegisterHandlerDirective("cadmod", func(confhdl httpcaddyfile.Helper) (
+		caddyhttp.MiddlewareHandler, error,
+	) {
+		c := CadMod{}
+		c.UnmarshalCaddyfile(confhdl.Dispenser)
+		return c, nil
+	})
 }
